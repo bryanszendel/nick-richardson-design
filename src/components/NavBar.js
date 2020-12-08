0a95382d1a7853a3777a19a82c648499ed406447
Bryan Szendel <bryan@bigarmor.com>
2020-12-07 22:37:45 -0500

    landing page hover svgs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'gatsby';
 
 import '../styles/global.scss';
@@ -6,17 +6,29 @@ import '../styles/landing.scss';
 import '../styles/navbar.scss';
 
 import dribbbleIcon from '../../static/navbar-svgs/dribbble_icon.svg';
+import dribbbleHover from '../../static/hover-svgs/dribbble_hover.svg';
 import linkedinIcon from '../../static/navbar-svgs/linkedin_icon.svg';
+import linkedinHover from '../../static/hover-svgs/linkedin_hover.svg';
 import emailIcon from '../../static/navbar-svgs/email_icon.svg';
-import cvIcon from '../../static/navbar-svgs/cv_icon.svg';
+import emailHover from '../../static/hover-svgs/email_hover.svg';
+import resumeIcon from '../../static/navbar-svgs/resume_icon.svg';
+import resumeHover from '../../static/hover-svgs/resume_hover.svg';
+
 import backArrow from '../../static/back_arrow.svg';
 
 export default function NavBar(props) {
+  const [dribbble, setDribbble] = useState(dribbbleIcon)
+  const [linkedin, setLinkedin] = useState(linkedinIcon)
+  const [email, setEmail] = useState(emailIcon)
+  const [resume, setResume] = useState(resumeIcon)
 
+  // const hoverHandler = (regImg, hoverImg) => {
+  //   onMouseEnter
+  // }
   return (
     <div className="navbar">
       {props.landing ? (
-        <div className="navbar-text underline">Nick Richardson</div>
+        <span className="underline navbar-text">Nick Richardson</span>
       ) : (
         <Link 
           to="/"
@@ -31,17 +43,48 @@ export default function NavBar(props) {
           <img style={{
             height: '48px', 
             width: '48px', 
-            // marginRight: '10px'
             }} src={backArrow} alt="Back arrow" 
           />
           <div className="navbar-text">All Projects</div>
         </Link>
       )}
-      <div className="navbar-items-container no-display">
-        <img className="navbar-items" src={dribbbleIcon} alt="Dribbble" />
-        <img className="navbar-items" src={linkedinIcon} alt="LinkedIn" />
-        <img className="navbar-items" src={emailIcon} alt="Email" />
-        <img className="navbar-items" src={cvIcon} alt="CV" />
+      <div className={props.landing ? "navbar-items-container" : "navbar-items-container no-display"}>
+        <div className="navbar-items">
+          <img 
+            className="dribbble" 
+            src={dribbble} 
+            alt="Dribbble" 
+            onMouseOver={() => setDribbble(dribbbleHover)} 
+            onMouseOut={() => setDribbble(dribbbleIcon)} 
+          />
+        </div>
+        <div className="navbar-items">
+          <img 
+            className="linkedin" 
+            src={linkedin} 
+            alt="LinkedIn"
+            onMouseEnter={() => setLinkedin(linkedinHover)} 
+            onMouseLeave={() => setLinkedin(linkedinIcon)} 
+          />
+        </div>
+        <div className="navbar-items">
+          <img 
+            className="email" 
+            src={email} 
+            alt="Email"
+            onMouseEnter={() => setEmail(emailHover)} 
+            onMouseLeave={() => setEmail(emailIcon)}
+          />
+        </div>
+        <div className="navbar-items">
+          <img 
+            className="resume" 
+            src={resume} 
+            alt="CV" 
+            onMouseEnter={() => setResume(resumeHover)} 
+            onMouseLeave={() => setResume(resumeIcon)}
+          />
+        </div>
       </div>
     </div>
   )
